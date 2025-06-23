399cc0d80b1eb545e64beb5b1dc32fc964e5f538
Sigireddy Balasai <101649913+SigireddyBalasai@users.noreply.github.com>
2025-06-23 23:54:51 +0530

    refactor: Improve formatting and readability of sign-in and sign-up pages

--- app/Concierge/TabsNavigation.tsx
+++ app/Concierge/TabsNavigation.tsx
@@ -3,18 +3,26 @@
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 type TabsNavigationProps = {
-    selectedTab: string;
-    setSelectedTab: (tab: string) => void;
-  };
-  
-  export function TabsNavigation({ selectedTab, setSelectedTab }: TabsNavigationProps) {
-    return (
-      <Tabs defaultValue="all" className="mb-6" onValueChange={setSelectedTab} value={selectedTab}>
-        <TabsList>
-          <TabsTrigger value="all">All no-shows </TabsTrigger>
-          <TabsTrigger value="favorites">Favorites</TabsTrigger>
-          <TabsTrigger value="shared">Shared</TabsTrigger>
-        </TabsList>
-      </Tabs>
-    );
-  }
+  selectedTab: string;
+  setSelectedTab: (tab: string) => void;
+};
+
+export function TabsNavigation({
+  selectedTab,
+  setSelectedTab,
+}: TabsNavigationProps) {
+  return (
+    <Tabs
+      defaultValue="all"
+      className="mb-6"
+      onValueChange={setSelectedTab}
+      value={selectedTab}
+    >
+      <TabsList>
+        <TabsTrigger value="all">All no-shows </TabsTrigger>
+        <TabsTrigger value="favorites">Favorites</TabsTrigger>
+        <TabsTrigger value="shared">Shared</TabsTrigger>
+      </TabsList>
+    </Tabs>
+  );
+}
